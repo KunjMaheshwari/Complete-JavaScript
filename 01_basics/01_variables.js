cbf0213ff6882f0d259f61b33c067019ea4a4b6b
Kunj Maheshwari <kunj.maheshwari2021@vitbhopal.ac.in>
2025-01-11 00:29:43 +0530

    Added 01_basic

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553; // locked
+
+let accountEmail = "kunjm@outlook.com"; // changable value
+
+var accountPassword = "test123"; // not used because of the scope.
+
+accountCity = "Vidisha";
+
+// accountId = 2;
+
+// accountEmail = "test@gmail.com";
+// accountPassword = "12345";
+
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountId);
+
+/*
+This is a multiline comment. 
+*/
+
+// console.table([accountId, accountEmail, accountPassword]);
