9652ea0912fbcb65803eaf08c4196cf58f083fdd
Kunj Maheshwari <kunj.maheshwari2021@vitbhopal.ac.in>
2025-01-15 19:33:18 +0530

    Added While and Do While Loop.

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+// let index = 0;
+// while(index <= 10){
+//     console.log(`Value of index is ${index}`);
+//     index += 1;
+// }
+
+let heroes = ["ironman", "spiderman", "thanos", "thor"];
+let arr = 0;
+
+while (arr < heroes.length) {
+    console.log(`Value is ${heroes[arr]}`);
+
+    arr += 1;
+}
+
+let score = 1;
+
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
